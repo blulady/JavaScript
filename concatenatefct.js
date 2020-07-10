416b0229deb45ef9f5f870b94cb71c48126d62c6
blulady <blulady@gmail.com>
2020-07-10 11:41:28 -0700

    step64

---
+++ concatenatefct.js
@@ -0,0 +1,5 @@
+function maybe() {
+    var sentence = "I am trying to learn";
+    sentence += " from this course.";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
---
+++ step64.js
@@ -0,0 +1,5 @@
+function work() {
+    var hmmmm = "function";
+    hmmmm += " actually work";
+    document.getElementById("step64").innerHTML = hmmmm;
+}
