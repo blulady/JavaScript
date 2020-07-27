22c9d5683e747245c245f0bc58390b94ce54c120
blulady <blulady@gmail.com>
2020-07-27 16:10:53 -0700

    fade in

--- let.js
+++ let.js
@@ -1,14 +1,4 @@
 var outside = "I am outside the building, I can't see in."
 document.write(outside);
 
-lostkitty();
-
-function lostkitty() {
-    var building = "The kitty is not on the second floor";
-    document.getElementById("lostkitty").innerHTML = building;
-    
-        if(5 > 4) {
-            let f = "The kitty is on the third floor";
-            document.getElementById("foundkitty").innerHTML = f;
-        }
-}
+where
